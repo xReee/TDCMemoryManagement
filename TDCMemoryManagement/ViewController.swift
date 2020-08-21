099c338dfc0e220a196538e2838e86e1d29c295e
renatafg <renata.faria@ifood.com.br>
2020-08-20 23:25:08 -0300

    Initial Commit

---
+++ TDCMemoryManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDCMemoryManagement
+//
+//  Created by Renata Faria on 20/08/20.
+//  Copyright © 2020 Renata Faria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
